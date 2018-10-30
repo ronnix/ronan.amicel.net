fab7e70600f1447aa3ff6eecef8249dd5720eb69
Ronan Amicel <ronan.amicel@gmail.com>
2018-10-30 18:28:22 +0100

    Update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,23 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Bonjour l’internet&nbsp;!</h1>
+    <p>Vous trouverez ici&nbsp;:</p>
+    <ul>
+        <li>tous mes <Link to="/articles/">articles</Link> de blog (certains en français et d’autres en anglais)&nbsp;;</li>
+        <li>les liens pour me retrouver sur
+            <a href="https://twitter.com/amicel" title="Twitter"><i className="icon-twitter" alt="Twitter" /></a>,
+            <a href="https://www.linkedin.com/in/ronanamicel" title="LinkedIn"><i className="icon-linkedin" alt="LinkedIn" /></a>, ou encore
+            <a href="https://github.com/ronnix" title="GitHub"><i className="icon-github" alt="GitHub" /></a>.
+        </li>
+    </ul>
+    <p>Et bientôt aussi&nbsp;:</p>
+    <ul>
+        <li>une petite biographie&nbsp;;</li>
+        <li>une liste de projets actuels et passés&nbsp;;</li>
+        <li>une invitation à travailler ensemble&nbsp;;</li>
+        <li>et peut-être d’autres choses encore...</li>
+    </ul>
   </div>
 )
 
