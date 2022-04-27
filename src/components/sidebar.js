f1fabd9d60e9094b9bbc9847d1ceca61c3bb0f02
Ronan Amicel <ronan.amicel@gmail.com>
2022-04-27 11:17:30 +0200

    Ajoute des liens vers Mastodon et Instagram

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,20 +3,22 @@ import { Link } from 'gatsby'
 
 import { active, bio, links, menu, portrait, sidebar, title} from './sidebar.module.css'
 
-import '../assets/fonts/fontello-771c82e0/css/fontello.css'
+import '../assets/fonts/fontello-ac7a8528/css/fontello.css'
 
 const Sidebar = () => (
   <header className={sidebar}>
     <section>
       <Link to="/" className={portrait}></Link>
       <h1 className={title}>Ronan Amicel</h1>
-      <p className={bio}>Développeur, geek, entrepreneur.</p>
+      <p className={bio}>Développeur, geek, curieux.</p>
     </section>
     <section className={links}>
       <ul>
+        <li><a href="https://mamot.fr/@ronnix" title="Mastodon"><i className="icon-mastodon" alt="Mastodon" /></a></li>
         <li><a href="https://twitter.com/amicel" title="Twitter"><i className="icon-twitter" alt="Twitter" /></a></li>
         <li><a href="https://www.linkedin.com/in/ronanamicel" title="LinkedIn"><i className="icon-linkedin" alt="LinkedIn" /></a></li>
-        <li><a href="https://github.com/ronnix" title="GitHub"><i className="icon-github" alt="GitHub" /></a></li>
+        <li><a href="https://www.instagram.com/ronnix/" title="Instagram"><i className="icon-instagram" alt="Instagram" /></a></li>
+        <li><a href="https://github.com/ronnix" title="GitHub"><i className="icon-github-circled" alt="GitHub" /></a></li>
       </ul>
     </section>
     <nav className={menu}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,17 +10,20 @@ const IndexPage = () => (
         <li>tous mes <Link to="/articles/">articles</Link> de blog (certains en français et d’autres en anglais)&nbsp;;</li>
         <li>des <Link to="/recettes/">recettes</Link> de cuisine&nbsp;;</li>
         <li>les liens pour me retrouver sur
+            <a href="https://mamot.fr/@ronnix" title="Mastodon"><i className="icon-mastodon" alt="Mastodon" /></a>,
             <a href="https://twitter.com/amicel" title="Twitter"><i className="icon-twitter" alt="Twitter" /></a>,
-            <a href="https://www.linkedin.com/in/ronanamicel" title="LinkedIn"><i className="icon-linkedin" alt="LinkedIn" /></a>, ou encore
-            <a href="https://github.com/ronnix" title="GitHub"><i className="icon-github" alt="GitHub" /></a>.
+            <a href="https://www.linkedin.com/in/ronanamicel" title="LinkedIn"><i className="icon-linkedin" alt="LinkedIn" /></a>,
+            <a href="https://www.instagram.com/ronnix/" title="Instagram"><i className="icon-instagram" alt="Instagram" /></a>,
+            ou encore
+            <a href="https://github.com/ronnix" title="GitHub"><i className="icon-github-circled" alt="GitHub" /></a>.
         </li>
     </ul>
-    <p>Et bientôt aussi&nbsp;:</p>
+    <p>Et un jour peut-être aussi&nbsp;:</p>
     <ul>
         <li>une petite biographie&nbsp;;</li>
         <li>une liste de projets actuels et passés&nbsp;;</li>
         <li>une invitation à travailler ensemble&nbsp;;</li>
-        <li>et peut-être d’autres choses encore...</li>
+        <li>et d’autres choses selon mon inspiration…</li>
     </ul>
   </Layout>
 )
